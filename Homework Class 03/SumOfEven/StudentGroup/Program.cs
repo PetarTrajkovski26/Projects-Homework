f73f836be1f915f1eebc941384b87d1b24f40091
PetarTrajkovski26 <101565269+PetarTrajkovski26@users.noreply.github.com>
2022-03-21 15:21:12 +0100

    Add files via upload

---
+++ Homework Class 03/SumOfEven/StudentGroup/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] studentsArrayG1 =new string[] { "Petar", "Marko", "Nikola", "Trajce", "Mario" };
+            string[] studentsArrayG2 =new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+
+           Console.WriteLine("Enter student group (Either 1 or 2)");
+           bool studentGroupSuccess = int.TryParse(Console.ReadLine(), out int studentGroup);
+           
+            if (studentGroupSuccess)
+            {
+                if (studentGroup == 1)
+                {
+                    foreach(string name in studentsArrayG1)
+                    {
+                        Console.WriteLine(name);
+                    }
+
+                }else if (studentGroup == 2)
+                {
+                    foreach(string name in studentsArrayG2)
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework Class 03/SumOfEven/SumOfEven/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System;
+class Program
+{
+    static void Main()
+    {
+        int size;
+        int evenSum = 0;
+
+        Console.WriteLine("Enter the size of the array:");
+        size = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = new int[size];
+
+        Console.WriteLine("Enter the array values:");
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        foreach (int value in arr)
+        {
+            if (value % 2 == 0)
+            {
+                evenSum += value;
+            }
+           
+        }
+
+        Console.WriteLine("Sum of all even numbers in the array is: " + evenSum);
+
+    }
+}
